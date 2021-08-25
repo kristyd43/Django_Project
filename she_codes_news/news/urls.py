53823fcfccd06c94fe67c7faf44ae8f40ca594a6
Kristy <kristyd463@gmail.com>
2021-08-25 16:27:23 +1000

    changes

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'news'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'), #/news
+    path('<int:pk>/', views.StoryView.as_view(), name='story'), #/news/story1,2,3 etc
+    path('add-story/', views.AddStoryView.as_view(),name='newStory') #/news/add-story
+    path('search-stories/<str:author-name>/', views.)
 ]
